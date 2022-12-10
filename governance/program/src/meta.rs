d9d4214341041ac2bf6880f5d3342e1dccda380a
Maximilian Schneider <mail@maximilianschneider.net>
2022-12-10 09:34:47 +0200

    adapt security txt to mango

--- governance/program/src/meta.rs
+++ governance/program/src/meta.rs
@@ -6,13 +6,13 @@ use solana_security_txt::security_txt;
 #[cfg(not(feature = "no-entrypoint"))]
 security_txt! {
     // Required fields
-    name: "Solana Program Library Governance Program",
-    project_url: "https://realms.today",
-    contacts: "email:security@solana.com",
-    policy: "https://github.com/solana-labs/solana/blob/master/SECURITY.md",
+    name: "Mango DAO Governance Program",
+    project_url: "https://dao.mango.markets/",
+    contacts: "email:blockworks@protonmail.com",
+    policy: "https://docs.mango.markets/mango-markets/bug-bounty",
 
     // Optional Fields
-    source_code: "https://github.com/solana-labs/solana-program-library",
+    source_code: "https://github.com/blockworks-foundation/solana-program-library",
     source_revision: env!("GITHUB_SHA"),
     source_release: env!("GITHUB_REF_NAME"),
     auditors: "Neodyme,OtterSec"
